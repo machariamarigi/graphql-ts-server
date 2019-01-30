13fae62cd0d86d5bc628f2e758808dc6a9c23dde
machariamarigi <machariamarigi@gmail.com>
2019-01-30 14:10:30 +0300

    Use the external schema and resolver files

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,10 @@
 import "reflect-metadata";
+import * as path from "path";
 import { GraphQLServer } from "graphql-yoga";
+import { importSchema } from "graphql-import";
+import { resolvers } from "./resolvers";
 
-const typeDefs = `
-  type Query {
-    hello(name: String): String!
-  }
-`;
-
-const resolvers = {
-  Query: {
-    hello: (_: any, { name }: any) => `Hello ${name || "World"}`
-  }
-};
+const typeDefs = importSchema(path.join(__dirname, "./schema.graphql"));
 
 const server = new GraphQLServer({ typeDefs, resolvers });
 server.start(() => console.log("Server is running on localhost:4000"));

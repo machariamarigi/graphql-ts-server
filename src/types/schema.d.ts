3541616ec99d27d9cbc19099808011bb8c495bcf
machariamarigi <machariamarigi@gmail.com>
2019-01-30 22:48:58 +0300

    Create types from schema

---
+++ src/types/schema.d.ts
@@ -0,0 +1,57 @@
+// tslint:disable
+// graphql typescript definitions
+
+declare namespace GQL {
+interface IGraphQLResponseRoot {
+data?: IQuery | IMutation;
+errors?: Array<IGraphQLResponseError>;
+}
+
+interface IGraphQLResponseError {
+/** Required for all errors */
+message: string;
+locations?: Array<IGraphQLResponseErrorLocation>;
+/** 7.2.2 says 'GraphQL servers may provide additional entries to error' */
+[propName: string]: any;
+}
+
+interface IGraphQLResponseErrorLocation {
+line: number;
+column: number;
+}
+
+interface IQuery {
+__typename: "Query";
+hello: string;
+}
+
+interface IHelloOnQueryArguments {
+name?: string | null;
+}
+
+interface IMutation {
+__typename: "Mutation";
+register: boolean | null;
+}
+
+interface IRegisterOnMutationArguments {
+user?: IUserInput | null;
+}
+
+interface IUserInput {
+firstName: string;
+lastName: string;
+password: string;
+email: string;
+}
+
+interface IUser {
+__typename: "User";
+firstName: string;
+lastName: string;
+password: string;
+email: string;
+}
+}
+
+// tslint:enable

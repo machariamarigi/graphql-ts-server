8e320b705bc536bb0610d5cbbbc609eb01607d24
machariamarigi <machariamarigi@gmail.com>
2019-02-07 22:59:17 +0300

    remove unnecessary migration

--- src/migration/1549365611453-create-users.ts
+++ src/migration/1549365611453-create-users.ts
@@ -3,7 +3,7 @@ import { MigrationInterface, QueryRunner } from "typeorm";
 export class createUsers1549365611453 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<any> {
     await queryRunner.query(
-      `CREATE TABLE "users" ("id" uuid NOT NULL, "firstName" character varying(255) NOT NULL, "lastName" character varying(255) NOT NULL, "email" character varying(255) NOT NULL, "password" text NOT NULL, CONSTRAINT "PK_a3ffb1c0c8416b9fc6f907b7433" PRIMARY KEY ("id"))`
+      `CREATE TABLE "users" ("id" uuid NOT NULL, "username" character varying(255) NOT NULL, "email" character varying(255) NOT NULL, "password" text NOT NULL, CONSTRAINT "PK_a3ffb1c0c8416b9fc6f907b7433" PRIMARY KEY ("id"))`
     );
   }
 

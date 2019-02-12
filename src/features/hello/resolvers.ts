c7bd00e348ff7dc7a4a5e6a2d4733193962ec593
machariamarigi <machariamarigi@gmail.com>
2019-02-12 23:00:13 +0300

    separate out resolvers into their feature folders

---
+++ src/features/hello/resolvers.ts
@@ -0,0 +1,8 @@
+import { GQL, ResolverMap } from "../../types";
+
+export const resolvers: ResolverMap = {
+  Query: {
+    hello: (_, { name }: GQL.IHelloOnQueryArguments) =>
+      `Hello ${name || "World"}`
+  },
+};

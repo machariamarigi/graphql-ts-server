03edf491a534f18296252bac8c1e4d315802939c
machariamarigi <machariamarigi@gmail.com>
2019-02-27 13:42:59 +0300

    have an universal format for yup validation errors

---
+++ src/utils/formatYupError.ts
@@ -0,0 +1,12 @@
+import { ValidationError } from "yup";
+
+export const formatYupError = (error: ValidationError) => {
+  const errors: Array<{ path: string; message: string }> = [];
+  error.inner.forEach(err => {
+    errors.push({
+      path: err.path,
+      message: err.message
+    });
+  });
+  return errors
+};

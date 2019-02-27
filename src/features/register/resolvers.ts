b2c225c069065e5852a2535f86245330170ad459
machariamarigi <machariamarigi@gmail.com>
2019-02-27 13:43:40 +0300

    intergrate yup to the registration resolvers

--- src/features/register/resolvers.ts
+++ src/features/register/resolvers.ts
@@ -1,9 +1,10 @@
-import { FatalError } from "./../../utils/fatalError";
+import { registrationValidationSchema } from './yupValidators';
 import * as bcrypt from "bcryptjs";
 
 import { GQL, ResolverMap } from "../../types";
 import { User } from "../../entity/User";
 import { EmailUsedError } from "./errors";
+
 
 export const resolvers: ResolverMap = {
   Query: {
@@ -11,36 +12,27 @@ export const resolvers: ResolverMap = {
   },
 
   Mutation: {
-    register: async (
-      _,
-      { userName, email, password }: GQL.IRegisterOnMutationArguments
-    ) => {
-      try {
-        const hashedPassword = await bcrypt.hash(password, 10);
-        const user = User.create({
-          username: userName,
-          email,
-          password: hashedPassword
-        });
+    register: async (_, args: GQL.IRegisterOnMutationArguments) => {
+      await registrationValidationSchema.validate(args);
+      const { userName, email, password } = args;
+      const hashedPassword = await bcrypt.hash(password, 10);
+      const user = User.create({
+        username: userName,
+        email,
+        password: hashedPassword
+      });
 
-        const emailAlreadyExits = await User.findOne({
-          where: { email },
-          select: ["id"]
-        });
+      const emailAlreadyExits = await User.findOne({
+        where: { email },
+        select: ["id"]
+      });
 
-        if (emailAlreadyExits) {
-          throw new EmailUsedError();
-        }
-
-        await user.save();
-        return true;
-      } catch (error) {
-        if (error.path) {
-          throw new FatalError({ data: { reason: error.message } });
-        } else {
-          throw error;
-        }
+      if (emailAlreadyExits) {
+        throw new EmailUsedError();
       }
+
+      await user.save();
+      return true;
     }
   }
 };

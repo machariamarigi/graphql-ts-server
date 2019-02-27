314257b9a0cd9f872da63896e8ffe82368543fee
machariamarigi <machariamarigi@gmail.com>
2019-02-27 13:40:13 +0300

    add input validation tests

--- src/features/register/register.test.ts
+++ src/features/register/register.test.ts
@@ -9,41 +9,57 @@ const passsword = "testpass";
 
 let getHost = () => "";
 
-const mutation = `
+const mutation = (userName: string, email: string, password: string) => `
   mutation {
-    register(userName: "${userName}", email: "${email}", password: "${passsword}")
+    register(userName: "${userName}", email: "${email}", password: "${password}")
   }
 `;
 
-describe("registers a user", async () => {
+describe("Registering a user", async () => {
   beforeAll(async () => {
     const app = await server();
     const { port } = app.address();
     getHost = () => `http://127.0.0.1:${port}`;
   });
 
-  test("mutation response", async () => {
-    const response = await request(getHost(), mutation);
+  test("returns mutation response", async () => {
+    const response = await request(getHost(), mutation(userName, email, passsword));
     expect(response).toEqual({ register: true });
   });
 
-  test("stored user", async () => {
+  test("stores a user", async () => {
     const users = await User.find({ where: { email } });
     const user = users[0];
     expect(user.email).toEqual(email);
   });
 
-  test("password hashing", async () => {
+  test("hashes the password", async () => {
     const users = await User.find({ where: { email } });
     const user = users[0];
     expect(user.password).not.toEqual(passsword);
   });
 
-  test("unique email", async () => {
+  test("doesn't duplicate used emails", async () => {
     try {
-      await request(getHost(), mutation);
+      await request(getHost(), mutation(userName, email, passsword));
     } catch (object) {
       expect(object.message).toContain("Email has already been used");
+    }
+  });
+
+  test("doesn't accept invalid emails", async () => {
+    try {
+      await request(getHost(), mutation(userName, "bad email", passsword));
+    } catch (object) {
+      expect(object.message).toContain("email must be a valid email");
+    }
+  });
+
+  test("doesn't accept short passwords", async () => {
+    try {
+      await request(getHost(), mutation(userName,email, "pass"));
+    } catch (object) {
+      expect(object.message).toContain("password must be at least 8 characters");
     }
   });
 });
